3f34237534cf0ecfd0381caee409f8f0c9c4b803
Jbodde <48587849+Jbodde@users.noreply.github.com>
2019-03-15 11:16:39 +0100

    Add files via upload

---
+++ artist-enricher-api-lib.js
@@ -0,0 +1,5 @@
+var artistEnricherApiLib = module.exports;
+
+// please go to https://developer.spotify.com/web-api/console/get-search-item/ and try out the API; request an OAuth token; copy and paste the value of the token in the next line
+artistEnricherApiLib.token = 'BQD8lIqUq2RM17VYj7B8gO6jgBhXKChuAyw3YfKaTqXTngK9lY98UgR3Cre-VtBdLFe4Wwm01ofmM0w41wERLuaELYed0fAMA_3H91Wd6sqTgSn715trVYtlk1FNjBsuFlno7e517CO5WzDttCzVcplfKrYwuGBEByvlAA';
+artistEnricherApiLib.spotifyAPI = 'https://api.spotify.com/v1';
